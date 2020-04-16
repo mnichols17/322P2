aa7c7c6c6bb715ec1a2f67b8ed3446332e37ed04
Mike <mike.nichols97@gmail.com>
2020-04-16 07:18:04 -0400

    API now called in App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import axios from 'axios';
 import TaskBoard from './TaskBoard';
 
-export default function App() {
-  return (
-    <div className="container-fluid" id="view">
-      <Router>
-        <div className="mt-3">
-          <Switch>
-              <Route path="/" component={TaskBoard} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  )
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      tasks: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get("https://my-json-server.typicode.com/mnichols17/project2/tasks")
+    .then(res => {
+        this.setState({
+          tasks: res.data
+        })
+      })
+    .catch(error => console.log(error))
+  }
+
+  render(){
+    return (
+      <div className="container-fluid" id="view">
+        <Router>
+          <div className="mt-3">
+            <Switch>
+                <Route path="/">
+                  <TaskBoard tasks={this.state.tasks} />
+                </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
+
+export default App
--- src/Components/TaskBoard.js
+++ src/Components/TaskBoard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+
 
 class TaskBoard extends React.Component {
 
@@ -10,27 +10,35 @@ class TaskBoard extends React.Component {
         }
     }
 
-    componentDidMount = async() => {
-        await axios.get("https://my-json-server.typicode.com/mnichols17/project2/tasks")
-            .then(res => {
-                this.setState({
-                    tasks: res.data
-                })
-            })
-            .catch(error => console.log(error))
+    renderCard = (task, status) => {
+        return task.status === status ? 
+        <div className="card my-2" key={task.id}>
+            <div className="card-body">
+                <h5 className="card-title">{task.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">ID: {task.id}</h6>
+            </div>
+        </div> : null
     }
 
     render() {
         return (
             <div className="container-fluid" id="taskBoard">
                 <div className="row">
-                    <div class="col" id="col0">
-                    {this.state.tasks.forEach(task => {
-                        console.log(task.status === 0 ? task : null)
-                    })}
+                    <div className="col" id="col0">
+                        <h2>To Do</h2>
+                        {this.props.tasks.map(task => this.renderCard(task, 0))}
                     </div>
-                    <div class="col" id="col1">
-
+                    <div className="col" id="col1">
+                        <h2>In Progress</h2>
+                        {this.props.tasks.map(task => this.renderCard(task, 1))}
+                    </div>
+                    <div className="col" id="col2">
+                        <h2>Review</h2>
+                        {this.props.tasks.map(task => this.renderCard(task, 2))}
+                    </div>
+                    <div className="col" id="col3">
+                        <h2>Done</h2>
+                        {this.props.tasks.map(task => this.renderCard(task, 3))}
                     </div>
                 </div>
             </div>
