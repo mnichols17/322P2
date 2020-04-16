dab84b28b8b50635ee82a8430f1099c31a00b40c
Mike <mike.nichols97@gmail.com>
2020-04-16 11:21:51 -0400

    Server assistance

---
+++ build/precache-manifest.e7843d00d0be6f10af0469e562bb6ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319d0c4ec72be18485a1800812403539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8ab1b5751ae8094ade",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2541e75f900565827355",
+    "url": "/static/css/main.ed5dc445.chunk.css"
+  },
+  {
+    "revision": "7a8ab1b5751ae8094ade",
+    "url": "/static/js/2.606d105a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.606d105a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2541e75f900565827355",
+    "url": "/static/js/main.7c3fcba0.chunk.js"
+  },
+  {
+    "revision": "308c93b411f196880a64",
+    "url": "/static/js/runtime-main.26da0443.js"
+  }
+]);
